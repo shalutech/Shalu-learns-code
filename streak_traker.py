a5efd506b493a1bc6e901491b5fd3f950742bf7f
shalutech <shalinishahin1207@gmail.com>
2026-05-19 14:43:09 +0530

    Create streak_traker.py
    Day 2:shalutech streak tracker completed

---
+++ streak_traker.py
@@ -0,0 +1,9 @@
+# Streak Tracker - 365 Days Challenge
+current_day = 2
+total_days = 365
+days_remaining = total_days - current_day
+progress_percentage = current_day / total_days * 100
+
+print(f"Day {current_day} completed ✅")
+print(f"{days_remaining} days remaining")
+print(f"I'm {progress_percentage:.2f}% closer to finish")
