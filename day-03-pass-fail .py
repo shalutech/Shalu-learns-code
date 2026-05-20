01fd14fe8f0b25032096c965fadd7116ca693f48
shalutech <shalinishahin1207@gmail.com>
2026-05-20 10:32:15 +0530

    Create day-03-pass-fail .py
    built my own pass-fail logic with if-elif-else✅

---
+++ day-03-pass-fail .py
@@ -0,0 +1,7 @@
+mark = 100
+if mark >= 95:
+    print( " CONGRATS 👏🏻  you are also done " )
+elif mark > 50:
+     print( " GOOD JOB 👍🏻 try to get 90 +" ) 
+else :
+      print( " TRY TO GET GOOD MARKS " )
